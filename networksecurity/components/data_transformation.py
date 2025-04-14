1048f480c9fb2ac17fb5ad91c165bc5cd40c1ebd
Krish0834 <krishjain0834@gmail.com>
2025-04-14 15:17:17 +0530

    trainer evaluation and mlflow

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -80,6 +80,16 @@ class DataTransformation:
             save_numpy_array_data(self.data_transformation_config.transformed_test_file_path,array=test_arr, )
             save_object(self.data_transformation_config.transformed_object_file_path,preprocessor_object,)
 
+            data_transformation_artifact = DataTransformationArtifact(
+                transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
+                transformed_test_file_path=self.data_transformation_config.transformed_test_file_path,
+                transformed_object_file_path=self.data_transformation_config.transformed_object_file_path
+            )
+
+            logging.info("Data Transformation artifact created")
+            return data_transformation_artifact
+
+
 
 
         except Exception as e:
